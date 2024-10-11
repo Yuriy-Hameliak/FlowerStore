a638c76370ca7f219b4798f49e45d151e46891d7
Yuriy Hameliak <yurka6677@gmail.com>
2024-10-11 17:28:36 +0300

    variable definition in wrong order

--- src/test/java/flower/store/StoreTest.java
+++ src/test/java/flower/store/StoreTest.java
@@ -8,20 +8,22 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class StoreTest {
-    private Store store;
-    private FlowerBucket bucketOne;
-    private FlowerBucket bucketTwo;
     private static final double SEPAL_LENGTH_ONE = 7.5;
     private static final double SEPALLENGTHTWO = 6.0;
     private static final double PRISEONE = 15;
     private static final double PRISTWO = 10;
     private static final int FIRST_QUANTITY = 10;
     private static final int SECOND_QUANTITY = 5;
+    private Store store;
+    private FlowerBucket bucketOne;
+    private FlowerBucket bucketTwo;
     @BeforeEach
     public void init() {
         store = new Store();
-        Flower rose = new Flower(FlowerType.ROSE, FlowerColor.RED, SEPAL_LENGTH_ONE, PRISEONE);
-        Flower tulip = new Flower(FlowerType.TULIP, FlowerColor.BLUE, SEPALLENGTHTWO, PRISTWO);
+        Flower rose = new Flower(FlowerType.ROSE,
+         FlowerColor.RED, SEPAL_LENGTH_ONE, PRISEONE);
+        Flower tulip = new Flower(FlowerType.TULIP,
+         FlowerColor.BLUE, SEPALLENGTHTWO, PRISTWO);
 
         FlowerPack rosePack = new FlowerPack(rose, FIRST_QUANTITY);
         FlowerPack tulipPack = new FlowerPack(tulip, SECOND_QUANTITY);
